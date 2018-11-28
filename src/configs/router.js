e310cb5a68f0e05f35b553ac3f94cc5342eebe9c
Calin <calinntamas@gmail.com>
2018-11-28 18:06:59 +0200

    add get profile route

--- src/configs/router.js
+++ src/configs/router.js
@@ -9,7 +9,7 @@ const authController = require('../controllers/authController');
 router.use('/', defaultController);
 router.use('/bookings', bookingController);
 router.use('/cities', cityController);
-router.use('/users', userController);
+router.use('/user', userController);
 router.use('/auth', authController);
 
 module.exports = router;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,71 +2,22 @@ const userRoute = require('express').Router();
 const userService = require('../services/userService');
 const Util = require('../commons/util');
 const STATUS_CODE = require('../commons/constants/statusCode');
+const authorize = require('../middlewares/authorize');
+const errorHandler = require('../utils/error-handler');
 
-userRoute.post('/', (request, response) => {
-  userService.save(request.body)
-    .then(id => {
-      response.header('Location', Util.generateLocationUri(request, id));
-      response.status(STATUS_CODE.CREATED);
-      response.end();
-    })
-    .catch(err => {
-      response.status(err.status);
-      response.json(err);
-    });
-});
-
-userRoute.get('/:id', (request, response) => {
-  userService.get(request.params.id)
-    .then(data => {
-      response.status(STATUS_CODE.OK);
-      response.json(data);
-    })
-    .catch(err => {
-      response.status(err.status);
-      response.json(err);
-    });
-});
-
-userRoute.delete('/:id', (request, response) => {
-  userService.delete(request.params.id)
-    .then(() => {
-      response.status(STATUS_CODE.NO_CONTENT);
-      response.end();
-    })
-    .catch(err => {
-      response.status(err.status);
-      response.json(err);
-    });
-});
-
-userRoute.put('/:id', (request, response) => {
-  userService.update(request.params.id, request.body)
-    .then(data => {
-      response.status(STATUS_CODE.OK);
-      response.json(data);
-    })
-    .catch(err => {
-      response.status(err.status);
-      response.json(err);
-    });
-});
-
-userRoute.get('/', (request, response) => {
-  userService.getAll(request.query.offset, request.query.limit)
-    .then(data => {
-      if (!data || data.size === 0) {
-        response.status(STATUS_CODE.NO_CONTENT);
-        response.end();
-      } else {
-        response.status(STATUS_CODE.OK);
-        response.json(data);
-      }
-    })
-    .catch(err => {
-      response.status(err.status);
-      response.json(err);
-    });
+userRoute.get('/profile', authorize, async (req, res) => {
+  try {
+    console.log('req.user', req.user);
+    const user = await userService.get(req.user.id);
+    return res
+      .status(STATUS_CODE.OK)
+      .json({
+        success: true,
+        data: user
+      });
+  } catch (err) {
+    errorHandler(err, req, res)
+  }
 });
 
 module.exports = userRoute;
